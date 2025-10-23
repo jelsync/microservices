bbaa1a2dd5bb6a4663b935b45e71f6815a1670fb
Jelsyn Johan Ceron Alvarez <jceron@bancatlan.hn>
2025-10-23 17:43:01 -0600

    Init project

---
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,36 @@
+package com.eazybytes.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Jelsyn Ceron",
+						email = "jelsync@yahoo.com",
+						url = "https://github.com/jelsync"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/jelsync"
+				)
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
